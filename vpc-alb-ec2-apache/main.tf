e554ceb0722d918dd88055ee37f1cd246f0ce25d
Justin Joseph <justin.joseph@version1.com>
2023-09-01 16:51:40 +0100

    WIP to sort demo setup

--- vpc-alb-ec2-apache/main.tf
+++ vpc-alb-ec2-apache/main.tf
@@ -24,57 +24,12 @@ provider "aws" {
   secret_key = var.aws_secret_key
 }
 
-
-resource "aws_network_interface" "eni-a" {
-  subnet_id       = module.vpc.subnet_a_id
-  private_ips     = ["10.0.1.15"]
-  security_groups = [aws_security_group.web-server-sg.id]
-}
-
-resource "aws_network_interface" "eni-b" {
-  subnet_id       = module.vpc.subnet_b_id
-  private_ips     = ["10.0.2.15"]
-  security_groups = [aws_security_group.web-server-sg.id]
-}
-
-resource "aws_network_interface" "eni-c" {
-  subnet_id       = module.vpc.subnet_c_id
-  private_ips     = ["10.0.3.15"]
-  security_groups = [aws_security_group.web-server-sg.id]
-}
-
-resource "aws_eip" "eip-a" {
-  vpc                       = true
-  network_interface         = aws_network_interface.eni-a.id
-  associate_with_private_ip = "10.0.1.15"
-}
-
-resource "aws_eip" "eip-b" {
-  vpc                       = true
-  network_interface         = aws_network_interface.eni-b.id
-  associate_with_private_ip = "10.0.2.15"
-}
-
-resource "aws_eip" "eip-c" {
-  vpc                       = true
-  network_interface         = aws_network_interface.eni-c.id
-  associate_with_private_ip = "10.0.3.15"
-}
-
 resource "aws_security_group" "web-server-sg" {
   name        = "web-server-sg"
-  description = "Allow TLS inbound traffic"
+  description = "Allow HTTP inbound traffic"
   vpc_id      = module.vpc.vpc_id
   ingress {
-    description = "TLS for VPC"
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Port 80 for VPC"
+    description = "HTTP Traffic"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -82,7 +37,7 @@ resource "aws_security_group" "web-server-sg" {
   }
 
   ingress {
-    description = "SSH for VPC"
+    description = "SSH Traffic"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -106,20 +61,16 @@ resource "aws_security_group" "web-server-sg" {
 resource "aws_instance" "server-a" {
   instance_type     = "t2.medium"
   availability_zone = "us-east-1a"
-  ami               = "ami-09e67e426f25ce0d7"
-  key_name          = "my-key"
-
-  network_interface {
-    device_index         = 0
-    network_interface_id = aws_network_interface.eni-a.id
-  }
-
+  ami               = "ami-053b0d53c279acc90"
+  subnet_id = module.vpc.private_subnet_a_id
+  key_name          = "demo-key"
   user_data = <<-EOF
                   #!/bin/bash
-                  apt -y update
-                  apt -y install apache2
-                  systemctl start apache2
-                  bash -c 'echo This webserver is BATMAN, created with terraform > /var/www/html/index.html'
+                  sudo apt -y update
+                  sudo apt -y install apache2
+                  sudo ufw allow 'Apache'
+                  sudo systemctl start apache2
+                  bash -c 'echo I AM BATMAN! > /var/www/html/index.html'
                   EOF
 
   tags = {
@@ -132,20 +83,16 @@ resource "aws_instance" "server-a" {
 resource "aws_instance" "server-b" {
   instance_type     = "t2.medium"
   availability_zone = "us-east-1b"
-  ami               = "ami-09e67e426f25ce0d7"
-  key_name          = "my-key"
-
-  network_interface {
-    device_index         = 0
-    network_interface_id = aws_network_interface.eni-b.id
-  }
-
+  ami               = "ami-053b0d53c279acc90"
+  subnet_id = module.vpc.private_subnet_b_id
+  key_name          = "demo-key"
   user_data = <<-EOF
                   #!/bin/bash
-                  apt -y update
-                  apt -y install apache2
-                  systemctl start apache2
-                  bash -c 'echo This webserver is SUPERMAN, created with terraform > /var/www/html/index.html'
+                  sudo apt -y update
+                  sudo apt -y install apache2
+                  sudo ufw allow 'Apache'
+                  sudo systemctl start apache2
+                  bash -c 'echo I AM SUPERMAN! > /var/www/html/index.html'
                   EOF
 
   tags = {
@@ -157,20 +104,16 @@ resource "aws_instance" "server-b" {
 resource "aws_instance" "server-c" {
   instance_type     = "t2.medium"
   availability_zone = "us-east-1c"
-  ami               = "ami-09e67e426f25ce0d7"
-  key_name          = "my-key"
-
-  network_interface {
-    device_index         = 0
-    network_interface_id = aws_network_interface.eni-c.id
-  }
-
+  ami               = "ami-053b0d53c279acc90"
+  subnet_id = module.vpc.private_subnet_c_id
+  key_name          = "demo-key"
   user_data = <<-EOF
                   #!/bin/bash
-                  apt -y update
-                  apt -y install apache2
-                  systemctl start apache2
-                  bash -c 'echo This webserver is just another one!, created with terraform > /var/www/html/index.html'
+                  sudo apt -y update
+                  sudo apt -y install apache2
+                  sudo ufw allow 'Apache'
+                  sudo systemctl start apache2
+                  bash -c 'echo I AM WONDER WOMAN! > /var/www/html/index.html'
                   EOF
 
   tags = {
@@ -186,12 +129,12 @@ resource "aws_lb" "webserver-alb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.web-server-sg.id]
-  subnets            = [module.vpc.subnet_a_id, module.vpc.subnet_b_id, module.vpc.subnet_c_id]
+  subnets            = [module.vpc.public_subnet_a_id, module.vpc.public_subnet_b_id, module.vpc.public_subnet_c_id]
 
   enable_deletion_protection = false
 
   tags = {
-    Environment = "production"
+    Environment = "demo"
   }
 }
 
--- vpc-alb-ec2-apache/modules/vpc/outputs.tf
+++ vpc-alb-ec2-apache/modules/vpc/outputs.tf
@@ -6,15 +6,26 @@ output "igw_id" {
   value = aws_internet_gateway.my-igw.id
 }
 
-output "subnet_a_id" {
+output "public_subnet_a_id" {
   value = aws_subnet.public-a.id
 }
 
-output "subnet_b_id" {
+output "public_subnet_b_id" {
   value = aws_subnet.public-b.id
 }
 
-output "subnet_c_id" {
+output "public_subnet_c_id" {
   value = aws_subnet.public-c.id
 }
 
+output "private_subnet_a_id" {
+  value = aws_subnet.private-a.id
+}
+
+output "private_subnet_b_id" {
+  value = aws_subnet.private-b.id
+}
+
+output "private_subnet_c_id" {
+  value = aws_subnet.private-c.id
+}
