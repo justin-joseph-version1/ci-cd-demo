a4c48f521ace75e3a3f72422a80aa7fbd6b96cfb
Justin Joseph <justin.joseph@version1.com>
2023-09-04 12:02:19 +0100

    Test s3 bucket backend

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,17 +5,15 @@ terraform {
       version = ">= 5.0.0"
     }
   }
-  #backend "s3" {
-  #  bucket = "justinjoeman-test-bucket"
-  #  key    = "mystatefile"
-  #  region = "us-east-1"
-  #  access_key = ""
-  #  secret_key = ""
-  #}
+  backend "s3" {
+    bucket = "jj-my-ci-cd-demo"
+    key    = "mystatefile"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" {
   region     = "us-east-1"
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  #access_key = var.aws_access_key
+  #secret_key = var.aws_secret_key
 }
